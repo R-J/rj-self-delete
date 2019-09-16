bfa8b32b0a9200eb2dab291fefe54fb8b8490010
R-J <robin.jurinka@gmx.net>
2019-09-16 10:41:47 +0200

    Initial commit

---
+++ SelfDeletePlugin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RJPlugins;
+
+use Gdn_Plugin;
+use Gdn;
+
+class SelfDeletePlugin extends \Gdn_Plugin {
+    // Add option to profile "Delete Account"
+    // Require re-entering password
+}
