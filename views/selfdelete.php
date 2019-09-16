e4ba47a4f78dabfe817ff05fb0d1e2158380a45f
R-J <robin.jurinka@gmx.net>
2019-09-16 16:46:27 +0200

    First view tryouts

---
+++ views/selfdelete.php
@@ -0,0 +1,17 @@
+<?php defined('APPLICATION') or die ?>
+<div class="FormTitleWrapper">
+    <h1 class="H"><?= $this->data('Title') ?></h1>
+    <?= $this->Form->open(), $this->Form->errors() ?>
+    <ul>
+        <li class="self-delete-warning">
+            <div class="DismissMessage AlertMessage">
+                <?php echo Gdn::translate('This will delete your account without any chance to recover it!') ?>
+            </div>
+        </li>
+        <li class="self-delete-password">
+            <?= $this->Form->label('Please enter your password to validate this action', 'Password') ?>
+            <?= $this->Form->input('Password', ['type' => 'password']) ?>
+        </li>
+    </ul>
+    <?= $this->Form->close('Delete Account', '', ['class' => 'Button Danger']) ?>
+</div>
