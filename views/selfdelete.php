eaf8db408a1084284e83432534b0bccd1b3ee46f
R-J <robin.jurinka@gmx.net>
2019-09-17 17:09:08 +0200

    Add Email as a field for azthentication

--- views/selfdelete.php
+++ views/selfdelete.php
@@ -8,6 +8,10 @@
                 <?php echo Gdn::translate('This will delete your account without any chance to recover it!') ?>
             </div>
         </li>
+        <li class="self-delete-email">
+            <?= $this->Form->label('Please enter your email address to validate this action', 'Email') ?>
+            <?= $this->Form->textBox('Email') ?>
+        </li>
         <li class="self-delete-password">
             <?= $this->Form->label('Please enter your password to validate this action', 'Password') ?>
             <?= $this->Form->input('Password', ['type' => 'password']) ?>
